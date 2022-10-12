6a4970717d3ce5fb68c9261d5c9b8e6cf60a5046
Nastia <anastasiia.petrovych@ucu.edu.ua>
2022-10-12 21:51:52 +0300

    Deleted

--- src/main/java/lotr/Randomizer.java
+++
@@ -1,9 +0,0 @@
-package lotr;
-import java.util.Random;
-
-public class Randomizer {
-    public int Randomizer(){
-        Random random = new Random();
-        int value = random.nextInt(11);
-        return value;
-    }}
