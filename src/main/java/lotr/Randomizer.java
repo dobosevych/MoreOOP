a052a6cda5f8e1cd2d359644577f96d5b1136c28
Nastia <anastasiia.petrovych@ucu.edu.ua>
2022-10-12 21:45:47 +0300

    final

---
+++ src/main/java/lotr/Randomizer.java
@@ -0,0 +1,9 @@
+package lotr;
+import java.util.Random;
+
+public class Randomizer {
+    public int Randomizer(){
+        Random random = new Random();
+        int value = random.nextInt(11);
+        return value;
+    }}
